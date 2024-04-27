07a54162277b1fe8493117fc7981939315e90edf
SyedaKaneesFathima <167081482+SyedaKaneesFathima@users.noreply.github.com>
2024-04-27 09:34:45 +0530

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,14 @@
+def is_leap(year):
+    leap = False
+    
+    if year % 4==0:
+        leaf = True
+    if year % 100 ==0:
+        if year % 400 ==0:
+            leaf = True
+        else:
+            leaf =False   
+    
+ 
+
+#Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
