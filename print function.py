222215a57d3fbe71984da1db56a0687d138e6011
SyedaKaneesFathima <167081482+SyedaKaneesFathima@users.noreply.github.com>
2024-04-27 09:33:38 +0530

    Create print function.py

---
+++ print function.py
@@ -0,0 +1,12 @@
+#The included code stub will read an integer n from studing without using any string method,try to print the following 123....n
+
+if _name_ == '_main_':
+    n = int(input().strip())
+    for i in range(1,n+1):
+        print(i,end="")
+
+output:-3
+Your Output (stdout)
+123
+Expected Output
+123
