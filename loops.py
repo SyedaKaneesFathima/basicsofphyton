955698da21bec4e9691aa8a73d1ba048c1b4792d
SyedaKaneesFathima <167081482+SyedaKaneesFathima@users.noreply.github.com>
2024-04-27 09:31:43 +0530

    Create loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+#The provided code stub reads and integer,n , from STDIN. For all non-negative integers i<n, print i^2:-
+if _name_ == '_main_':
+    n = int(input().strip())
+    for i in range(n):
+        print(i**2)
+output
+0
+1
+4
+9
+16
